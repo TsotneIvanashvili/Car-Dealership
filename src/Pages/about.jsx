b59ba35a82fd98477ad77681a28a5c744366aff5
TsotneIvanashvili <Tsotneivanashvili@gmail.com>
2026-06-19 18:37:48 +0400

    main

--- src/Pages/about.jsx
+++ src/Pages/about.jsx
@@ -1,5 +1,3 @@
-import { Link } from "react-router-dom";
-import CardSwap, { Card } from "../components/Animated/CardStack";
 import WorldMap from "../components/WorldMap";
 
 const cardHeaders = [
@@ -185,58 +183,7 @@ const About = () => {
         </div>
       </section>
 
-      
-<section className="relative h-175 overflow-hidden bg-[#050914] pt-20 text-white md:px-16 lg:px-24">
-        <div className="mx-auto flex flex-col min-h-190 max-w-7xl gap-12 ">
-          <div className="relative flex flex-col justify-start z-20">
-            <h1 className="max-w-xl text-5xl font-black uppercase leading-[0.9] tracking-tight md:text-7xl">
-              What Do We Offer?
-            </h1>
-
-            <p className="mt-6 max-w-lg text-lg leading-8 text-[#A7B0AD]">
-              We offer a carefully selected range of premium vehicles, built for
-              drivers who want style, comfort, and performance in one place.
-              From powerful sports cars to refined daily drives, every car is
-              chosen to deliver quality, confidence, and a better driving
-              experience.
-            </p>
-
-            <Link to={"/cars"} className="rounded-full border-2 border-white  px-8 py-4 font-semibold bg-white text-black w-fit mt-10 hover:text-white hover:bg-transparent transition">
-              View Cars
-            </Link>
-          </div>
-
-          <div className="relative mt-20 overflow-visible">
-            <CardSwap
-              width={790}
-              height={700}
-              cardDistance={50}
-              verticalDistance={70}
-              delay={3000}
-              pauseOnHover={false}
-              skewAmount={4}
-              easing="elastic"
-            >
-              {cardHeaders.map((item, i) => (
-                <Card
-                  key={i}
-                  className="overflow-hidden rounded-3xl border border-white/60 bg-[#090A10] shadow-2xl"
-                >
-                  <div className="flex h-16 items-center gap-4 border-b border-white/70 bg-black/70 px-7 backdrop-blur-md">
-                    <i
-                      className={`fa-solid ${item.icon} text-xl text-white/90`}
-                    ></i>
-
-                    <h4 className="text-2xl font-bold tracking-tight text-white/90">
-                      {item.title}
-                    </h4>
-                  </div>
-                </Card>
-              ))}
-            </CardSwap>
-          </div>
-        </div>
-      </section>
+   
 
     </main>
   );
